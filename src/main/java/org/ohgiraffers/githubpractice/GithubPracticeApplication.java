91fc9669da6786a18b32687cc628339f87f60520
ruah0807 <kimhk0315@gmail.com>
2024-02-16 10:46:52 +0900

    [feat] : application 실행시 hello world 출력
    세부사항 작성

--- src/main/java/org/ohgiraffers/githubpractice/GithubPracticeApplication.java
+++ src/main/java/org/ohgiraffers/githubpractice/GithubPracticeApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubPracticeApplication {
 
+
+
     public static void main(String[] args) {
+
+        System.out.println("Hello World ^_^");
+
         SpringApplication.run(GithubPracticeApplication.class, args);
     }
 
